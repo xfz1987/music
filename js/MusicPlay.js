837b1e839eb27871fb7f2ff714eef01db3031c7f
gzf <281345774@qq.com>
2017-07-08 01:57:47 +0800

    change face and music

--- js/MusicPlay.js
+++ js/MusicPlay.js
@@ -1,20 +1,21 @@
 ;(function(){
 	var MusicPlay = function(options){
 		this.backBtn = options.backBtn;
-    	this.playBtn = options.playBtn;
-    	this.forwardBtn = options.forwardBtn;
-    	this.titleNode = options.titleNode;
-    	this.authorNode = options.authorNode;
-    	this.timeNode = options.timeNode;
-    	this.progressBarNode = options.progressBarNode;
-    	this.progressNowNode = options.progressNowNode;
-    	this.icon = options.icon;
-    	this.iconStatusClass = options.iconStatusClass;
-    	this.timer = null;
-    	this.musicIndex = 0;
-    	this.musicList = [];
-    	this.music = null;
-    	this._init();
+    this.playBtn = options.playBtn;
+    this.forwardBtn = options.forwardBtn;
+    this.titleNode = options.titleNode;
+    this.authorNode = options.authorNode;
+    this.timeNode = options.timeNode;
+    this.progressBarNode = options.progressBarNode;
+    this.sliderNode = options.sliderNode;
+    this.downLoadNode = options.downLoadNode;
+    this.icon = options.icon;
+    this.iconStatusClass = options.iconStatusClass;
+    this.timer = null;
+    this.musicIndex = 0;
+    this.musicList = [];
+    this.music = null;
+    this._init();
 	};
 	MusicPlay.prototype = {
 		_init: function(){
@@ -30,72 +31,74 @@
 		},
 		_getMusic: function(callback){
 			var xhr = new XMLHttpRequest(),_this = this;
-      		xhr.open('get', 'music.json', true);
-      		xhr.send();
-      		xhr.onload = function() {
-      		  if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
-      		    callback(JSON.parse(xhr.responseText));
-      		  }
-      		};
+      xhr.open('get', 'music.json', true);
+      xhr.send();
+      xhr.onload = function() {
+        if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
+          callback(JSON.parse(xhr.responseText));
+        }
+      };
 		},
 		_loadMusic: function(songObj){
 			this.music.src = songObj.src;
 			this.titleNode.innerText = songObj.title;
 			this.authorNode.innerText = songObj.auther;
+      this.downLoadNode.href = songObj.src;
+      this.downLoadNode.download = songObj.title + '.mp3';
 		},
 		_initEvent: function(){
 			var _this = this;
 			this.playBtn.addEventListener('click', function(){
-      			_this.icon.classList.contains(_this.iconStatusClass.play) ? _this.music.play() : _this.music.pause();
-      			_this.icon.classList.toggle(_this.iconStatusClass.play);
-      			_this.icon.classList.toggle(_this.iconStatusClass.pause);
-			}, false);
+       	_this.icon.classList.contains(_this.iconStatusClass.play) ? _this.music.play() : _this.music.pause();
+       	_this.icon.classList.toggle(_this.iconStatusClass.play);
+       	_this.icon.classList.toggle(_this.iconStatusClass.pause);
+			 }, false);
 
-      		this.forwardBtn.addEventListener('click', function(){
-      			_this.loadNPMusic('next');
-      		}, false);
+      this.forwardBtn.addEventListener('click', function(){
+      	_this.loadNPMusic('next');
+      }, false);
 
-      		this.backBtn.addEventListener('click', function(){
-      			_this.loadNPMusic('prev');
-      		}, false);
+      this.backBtn.addEventListener('click', function(){
+      	_this.loadNPMusic('prev');
+      }, false);
 
-      		this.music.addEventListener('ended', function(){
-      			_this.loadNPMusic('next');
-      		}, false);
+      this.music.addEventListener('ended', function(){
+      	_this.loadNPMusic('next');
+      }, false);
 
-      		this.music.addEventListener('playing', function(){
-      			_this.timer = setInterval(function(){
-        			_this.updateProgress();
-      			}, 1000)
-      		}, false);
+      this.music.addEventListener('playing', function(){
+      	_this.timer = setInterval(function(){
+      		_this.updateProgress();
+      	}, 1000)
+      }, false);
 
-      		this.music.addEventListener('pause', function(){
-      			clearInterval(_this.timer);
-      		}, false);
+      this.music.addEventListener('pause', function(){
+      	clearInterval(_this.timer);
+      }, false);
 
-      		this.progressBarNode.addEventListener('click', function(e){
-      			var percent = e.offsetX / parseInt(getComputedStyle(this).width);
-      			_this.music.currentTime = percent * _this.music.duration;
-      			_this.progressNowNode.style.width = percent * 100 + "%";
-      		}, false);
+      this.progressBarNode.addEventListener('click', function(e){
+      	var percent = e.offsetX / parseInt(getComputedStyle(this).width);
+      	_this.music.currentTime = percent * _this.music.duration;
+      	_this.sliderNode.style.left = percent * 100 + "%";
+      }, false);
 		},
 		loadNPMusic(type){
 			if(type == 'next'){
 				this.musicIndex++;
-      			this.musicIndex = this.musicIndex % this.musicList.length;
+      	this.musicIndex = this.musicIndex % this.musicList.length;
 			}else{
 				this.musicIndex--;
-     	 		this.musicIndex = (this.musicIndex + this.musicList.length) % this.musicList.length;
+     	 	this.musicIndex = (this.musicIndex + this.musicList.length) % this.musicList.length;
 			}
 			this._loadMusic(this.musicList[this.musicIndex]);
 		},
 		updateProgress: function(){
 			var percent = (this.music.currentTime / this.music.duration) * 100 + '%';
-      		this.progressNowNode.style.width = percent;
-      		var minutes = parseInt(this.music.currentTime / 60),
-      			seconds = parseInt(this.music.currentTime % 60) + '';
-      		seconds = seconds.length == 2 ? seconds : '0' + seconds;
-      		this.timeNode.innerText = minutes + ':' + seconds;
+      this.sliderNode.style.left = percent;
+      var minutes = parseInt(this.music.currentTime / 60),
+          seconds = parseInt(this.music.currentTime % 60) + '';
+      seconds = seconds.length == 2 ? seconds : '0' + seconds;
+      this.timeNode.innerText = minutes + ':' + seconds;
 		}
 	};
 	MusicPlay.init = function(config){
